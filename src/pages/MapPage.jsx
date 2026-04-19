129ec257c1c0a81715318ef2f6f38940220f531b
averylinn <linna@sonoma.edu>
2026-04-19 13:15:48 -0700

    Update MapPage.jsx

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,6 +1,46 @@
 import { useState } from "react";
+import L from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import { useNavigate } from "react-router-dom";
+import "leaflet/dist/leaflet.css";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
+const MOCK_CASES = [
+  {
+    id: "1",
+    name: "Person 1",
+    age: 17,
+    lat: 38.4405,
+    lng: -122.7144,
+    lastSeen: "Santa Rosa, CA",
+    lastSeenDate: "2026-04-10",
+  },
+  {
+    id: "2",
+    name: "Person 2",
+    age: 22,
+    lat: 38.2919,
+    lng: -122.4580,
+    lastSeen: "Petaluma, CA",
+    lastSeenDate: "2026-04-12",
+  },
+  {
+    id: "3",
+    name: "Person 3",
+    age: 14,
+    lat: 38.2324,
+    lng: -122.6367,
+    lastSeen: "Rohnert Park, CA",
+    lastSeenDate: "2026-04-08",
+  },
+];
 
 function LocationMarker({ setMarkerPosition }) {
   useMapEvents({
@@ -45,9 +85,25 @@ export default function MapPage() {
 
         <LocationMarker setMarkerPosition={setMarkerPosition} />
 
+        {MOCK_CASES.map((person) => (
+          <Marker key={person.id} position={[person.lat, person.lng]}>
+            <Popup>
+              <div>
+                <strong>{person.name}</strong>
+                <br />
+                Age: {person.age}
+                <br />
+                Last seen: {person.lastSeen}
+                <br />
+                Date: {person.lastSeenDate}
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+
         {markerPosition && (
           <Marker position={markerPosition}>
-            <Popup>Last seen location</Popup>
+            <Popup>New reported location</Popup>
           </Marker>
         )}
       </MapContainer>
